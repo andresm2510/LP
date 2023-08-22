e3a288d13e0bad9635e7dc97c11f9ef818bfd234
andresm2510 <andre.smendes.20231@poli.ufrj.br>
2023-08-22 14:13:18 -0300

    correcao1

--- aulaLP1708.py
+++ aulaLP1708.py
@@ -1,13 +1,16 @@
-   from flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-   return "<h1>Ola</h1>"
+    return "<h1>Ola</h1>"
+
 
 @app.route("/buda")
 def hellodenovo():
-   return "<h1>Sou outra pagina<h1>"
+    return "<h1>Sou outra pagina<h1>"
+
 
 app.run()
